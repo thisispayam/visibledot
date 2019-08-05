17b5277cfa4aeebe2025472081bfe386a7dc25bf
Payam <thisispayam@gmail.com>
2019-08-05 14:33:30 -0700

    Cart item component and its connection with cart items reducer

--- src/redux/cart/CartUtils.js
+++ src/redux/cart/CartUtils.js
@@ -4,12 +4,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
 
     if (existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id 
-            ?{ ... cartItem, quantity: cartItem.quantity + 1}
-            : cartItem
-            )
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
-}
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
